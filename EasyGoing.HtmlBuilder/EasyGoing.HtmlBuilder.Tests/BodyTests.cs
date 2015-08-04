7ef9fa0d35bfe60e4f5ec291eb6a53ffe5e471a6
Dmytro Chumachenko <chumachenko84@hotmail.com>
2015-08-05 00:07:23 +0300

    Added Body Getstring test

---
+++ EasyGoing.HtmlBuilder/EasyGoing.HtmlBuilder.Tests/BodyTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyGoing.HtmlBuilder.Tests
+{
+    [TestFixture]
+    class BodyTests
+    {
+        [Test]
+        public void CreateBody_GetString_ReturnsEmptyTag()
+        {
+            Body body = new Body();
+
+            string actual = body.GetString();
+            string expected = "<body></body>";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
